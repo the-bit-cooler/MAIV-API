7e6640548527601c359972c09e7a2a85f063e4da
The Bit Cooler <thebitcooler@icloud.com>
2025-11-02 13:58:42 +0000

    fix: future-proof by returning user email from validated session token

--- Services/JWT/ValidateSessionToken.cs
+++ Services/JWT/ValidateSessionToken.cs
@@ -1,13 +1,16 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ScripturAI.Services;
 
 public partial class JwtService
 {
-  internal static void ValidateSessionToken(string token)
+  internal static string ValidateSessionToken(string token)
   {
-    new JwtSecurityTokenHandler().ValidateToken(token, new TokenValidationParameters
+    JwtSecurityTokenHandler tokenHandler = new();
+
+    ClaimsPrincipal principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
     {
       ValidateIssuerSigningKey = true,
       IssuerSigningKey = GetSecurityKey(),
@@ -18,5 +21,11 @@ public partial class JwtService
       ValidateLifetime = true,
       ClockSkew = TimeSpan.Zero
     }, out SecurityToken validatedToken);
+
+    string? email = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+    if (string.IsNullOrEmpty(email)) throw new Exception("Invalid or expired session.");
+
+    return email;
   }
 }
