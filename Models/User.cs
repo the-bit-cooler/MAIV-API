1fd3e36516e1c113eed914e6199233e4c30145e2
The Bit Cooler <thebitcooler@icloud.com>
2025-11-06 23:55:31 +0000

    chore(user): remove unused passkeys array from user object as it will not be supported

--- Models/User.cs
+++ Models/User.cs
@@ -5,5 +5,4 @@ public class User
   public string? id { get; set; } // email
   public string collection => nameof(User); // partition key
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-  public List<Passkey>? Passkeys { get; set; } = [];
 }
