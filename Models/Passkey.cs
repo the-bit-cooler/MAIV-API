362a387d33d23a2464f3453706a107c2d6ed2f7b
The Bit Cooler <thebitcooler@icloud.com>
2025-11-07 00:17:29 +0000

    fix(auth): remove unused passkey class as passkeys will not be supported

--- Models/Passkey.cs
+++
@@ -1,8 +0,0 @@
-namespace ScripturAI.Models;
-
-public class Passkey
-{
-  public string Id { get; set; } = string.Empty;
-  public string PublicKey { get; set; } = string.Empty;
-  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
